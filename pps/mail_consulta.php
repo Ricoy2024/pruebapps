d9d5f49452809d7c90d3549897bb3acbc52e9cc7
lucasolivieri2024 <lucasmaximilianoolivieri@gmail.com>
2024-09-15 21:29:11 -0300

    1

--- pps/mail_consulta.php
+++ pps/mail_consulta.php
@@ -1,18 +1,8 @@
 <?php include('public/view/layout/header.php') ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- php mailer-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">    
-    
-</head>
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <body>
     <div class="container">
         <div class="row">
@@ -52,11 +42,11 @@
 
     </section>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+
+</body>
+  <!-- scrip php mailer-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-</body>
-
-</html>
 
 <?php include('public/view/layout/footer.php') ?>
--- pps/public/view/layout/header.php
+++ pps/public/view/layout/header.php
@@ -23,7 +23,11 @@
     <script src="https://code.jquery.com/jquery-3.3.1.js"> </script>
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 
+ 
+
+  
             
+
    </head>
    <body class="bg-drak2">
     <header class="bg-dark">
